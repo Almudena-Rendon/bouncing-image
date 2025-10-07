9c961d43924b07db1c3a7f376d1bfacd0ba1ffbb
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2025-10-07 11:53:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <img src="/astronaut.png" alt="Astronaut" className="astronaut" />
+      </div>
+    </>
+  )
+}
+
+export default App
